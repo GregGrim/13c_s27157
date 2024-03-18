b3a3b15a01fe763675b8a92ff06755d3766be87d
GregGrim <116919791+GregGrim@users.noreply.github.com>
2024-03-18 12:34:52 +0100

    task5

--- lab3.py
+++ lab3.py
@@ -2,9 +2,17 @@ from math import sqrt
 
 
 class SquareGenerator:
+    class MyException(Exception):
+        pass
+
     def get_squares(self, start: int, end: int) -> list[float]:
+        if start > end:
+            raise self.MyException()
         return [sqrt(i) for i in range(start, end)]
 
 
 gen = SquareGenerator()
-print(gen.get_squares(1, 11))
+try:
+    print(gen.get_squares(1, 11))
+except SquareGenerator.MyException:
+    print("start number is bigger than end number")
